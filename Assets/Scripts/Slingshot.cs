5ae036587d8d0c31bf215737cb7fc742d0dc0a0c
Mr. Satan <0421jamesheo@gmail.com>
2020-02-12 21:55:09 -0500

    Projectile now spawns on click in slingshot
    Projectile prefab now cloned upon click

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Set in	Inspector")]//	a
+
+    public GameObject prefabProjectile;
+    //	fields	set	dynamically
+    [Header("Set Dynamically")]
+
     public GameObject launchPoint;
+    public Vector3 launchPos;//	b
+    public GameObject projectile;//	b
+    public bool aimingMode;//	b
 
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");//	a
         launchPoint = launchPointTrans.gameObject;
-        launchPoint.SetActive(false);                                                                                                                                                                  //	b
+        launchPoint.SetActive(false);//	b
+        launchPos = launchPointTrans.position;//c
     }
     void OnMouseEnter()
     {
         //print("Slingshot:OnMouseEnter()");
-        launchPoint.SetActive(true);                                                                                                                                                                          //	b
+        launchPoint.SetActive(true);//	b
     }
     void OnMouseExit()
     {
         //print("Slingshot:OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {//	d                                                                                                                                                                                                               //	The	player	has	pressed	the	mouse	button	while	over	Slingshot
+        aimingMode = true;
+        //	Instantiate	a	Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //	Start	it	at	the	launchPoint
+        projectile.transform.position = launchPos;
+        //	Set	it	to	isKinematic	for	now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     // Start is called before the first frame update
